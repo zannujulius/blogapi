6ed1b14a4186650f8bc3de3f301da2f5400e8d2c
zannujulius <zannujulius14@gmail.com>
2023-05-06 12:45:11 +0100

    changed response

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ const app = express();
 const PORT = 5000;
 
 app.get("/", (req, res, next) => {
-  res.send("Hello world");
+  res.json({
+    age: 23,
+    name: "julius",
+  });
 });
 
 app.listen(PORT, () => {
